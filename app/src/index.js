8939fbf016ea9dac03f3e6fb4c29499023f13677
Tatsiana Kulikouskaya <tatsiana@pedanov.ru>
2021-05-11 18:29:11 +0300

    Added fetch and display clicked movie

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,14 @@
+/* eslint-disable no-unused-expressions */
 /* eslint-disable no-use-before-define */
 /* eslint-disable camelcase */
-const API_URL = 'https://api.themoviedb.org/3/discover/movie';
+const API_URL = 'https://api.themoviedb.org/3/';
 const API_KEY = 'd32f4d7dd5677076b7f9396a736b2491';
 const IMG_BASE_URL = 'https://image.tmdb.org/t/p/w500';
 
 const moviesList = document.querySelector('.movies-list');
+
+const movieSection = document.querySelector('.movie');
+const movieContent = document.querySelector('.movie-content');
 
 const sort = document.querySelector('.select-sort');
 const pagination = document.querySelector('.pagination');
@@ -22,7 +26,7 @@ async function showApp(page = 1) {
 }
 
 async function fetchMovies(sortby, page) {
-  const url = `${API_URL}?&vote_count.gte=1000&api_key=${API_KEY}&language=en-US&sort_by=${sortby}&page=${page}`;
+  const url = `${API_URL}discover/movie?&vote_count.gte=1000&api_key=${API_KEY}&language=en-US&sort_by=${sortby}&page=${page}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
@@ -32,13 +36,15 @@ function displayMovies(movies) {
   moviesList.innerHTML = '';
   movies.forEach((movie) => {
     const {
-      title, poster_path, release_date, vote_average,
+      title, id, poster_path, release_date, vote_average,
     } = movie;
     const movieItem = document.createElement('li');
+    movieItem.setAttribute('data-reference', id);
+    movieItem.className = 'movie-item';
     movieItem.innerHTML = `
         <img src=${IMG_BASE_URL + poster_path} alt ="${title}">
-        <p>${release_date.substring(0, 4)}</p>
-        <span>${vote_average}</span>
+        <p>Release: ${release_date.substr(0, 7).split('-').reverse().join('/')}</p>
+        <span>Rating: ${vote_average}</span>
         <h2>${title}</h2>
           `;
     moviesList.appendChild(movieItem);
@@ -50,14 +56,14 @@ sort.addEventListener('change', () => {
   showApp(current.innerHTML);
 });
 
-document.addEventListener('click', (e) => {
-  if (e.target && e.target.classList.contains('page__btn--next')) {
+pagination.addEventListener('click', (e) => {
+  if (e.target.classList.contains('page__btn--next')) {
     const current = document.querySelector('.page__item--current');
     showApp(+current.innerHTML + 1);
-  } else if (e.target && e.target.classList.contains('page__btn--prev')) {
+  } else if (e.target.classList.contains('page__btn--prev')) {
     const current = document.querySelector('.page__item--current');
     showApp(+current.innerHTML - 1);
-  } else if (e.target && e.target.classList.contains('page__item')) {
+  } else if (e.target.classList.contains('page__item')) {
     showApp(e.target.innerHTML);
   }
 });
@@ -108,4 +114,69 @@ function displayPagesList(page, arr, total) {
     pagination.appendChild(next);
     window.scrollTo(0, 0);
   }
+}
+
+moviesList.addEventListener('click', (e) => {
+  if (e.target.classList.contains('movie-item') || e.target.parentElement.classList.contains('movie-item')) {
+    const movieId = e.target.classList.contains('movie-item') ? (e.target.getAttribute('data-reference')) : (e.target.parentElement.getAttribute('data-reference'));
+    showMovie(movieId);
+  }
+});
+
+async function showMovie(id) {
+  const data = await fetchMovie(id);
+  displayMovie(data);
+}
+
+async function fetchMovie(id) {
+  const url = `${API_URL}movie/${id}?api_key=${API_KEY}&language=en-US`;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+function displayMovie(movie) {
+  movieSection.style.display === 'block' ? movieSection.style.display = 'none' : movieSection.style.display = 'block';
+  const {
+    title, original_title, poster_path, overview,
+    popularity, release_date, vote_average, vote_count, genres,
+  } = movie;
+  movieContent.innerHTML = `
+    <div class="movie-page__row">
+      <div class="movie-page__img">
+          <img src=${IMG_BASE_URL + poster_path} alt="${original_title}">
+      </div>
+      <div class="movie-page__text">
+        <h2>${title}</h2>
+        <p>${overview}</p>
+        <h4><strong>Popularity:</strong> ${Math.round(popularity)}</h4>
+        <h4><strong>Release date:</strong> ${release_date.split('-').reverse().join('/')}</h4>
+        <h4><strong>Rating:</strong> ${vote_average}</h4>
+        <h4><strong>Vote count:</strong> ${vote_count}</h4>
+        <ul>Genres: ${getGenres(genres)}</ul>
+      </div>
+      <button class="close-button">X</button>
+    </div>
+      `;
+  document.body.style.overflow = 'hidden';
+  document.querySelector('html').scrollTop = window.scrollY;
+}
+
+function getGenres(genresArr) {
+  let genres = '';
+  genresArr.forEach((el) => {
+    genres += `<li>${el.name}</li>`;
+  });
+  return genres;
+}
+
+window.addEventListener('click', (e) => {
+  if (e.target.classList.contains('close-button') || e.target.classList.contains('movie')) {
+    closeMovie();
+  }
+});
+
+function closeMovie() {
+  movieSection.style.display = 'none';
+  document.body.style.overflow = null;
 }
